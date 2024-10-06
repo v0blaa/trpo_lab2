5355b4274fa3e5b3d7c8d2d69710c07bf6f221d5
Анастасия Ищенко <nastyaisc.kts.studio@gmail.com>
2024-10-06 22:26:19 +0300

    fixed main.c

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
-#include <stdio.h>
-int main(int argc, char **argv) {
-    printf("Hello world!\n");
+#include <string.h>int main(int argc, char **argv) {
+    char name[255];
+fgets(name, 255, stdin);
+printf("length = %d\n", strlen(name)); /* debug line */ name[strlen(name)-1] = '\0'; /* remove the newline at the end */
+    printf("Hello %s!\n", name);
 return 0; }
