a39a0ddcc728a5acafe271ad371a8d055772ac40
Анастасия Ищенко <nastyaisc.kts.studio@gmail.com>
2024-10-08 14:22:31 +0300

    commit for rebase

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include "askname.h"
+
+// new changes
 int main(int argc, char **argv) {
     char first[255], last[255];
     askname(first, last);
     printf("Hello, %s %s!\n", first, last);
     return 0;
 }
-// I love TRPO
